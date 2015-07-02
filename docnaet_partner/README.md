239fdb83071aa733849fc34560f8f8b86c69e110
nicothebrush <nicola.riolini@gmail.com>
2015-07-02 11:37:01 +0200

    [ADD] docnaet-partner: Add module for integrare document in partner

---
+++ docnaet_partner/README.md
@@ -0,0 +1,7 @@
+# micronaet-docnaet
+Micronaet - Docnaet Partner integration
+=======================================
+
+Integrate docnaet document in partner form
+Used for activate a page in partner for have docnaet document available there
+
---
+++ docnaet_partner/__openerp__.py
@@ -0,0 +1,44 @@
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Docnaet: Partner integration',
+    'version': '0.1',
+    'category': 'Document / Partner',
+    'description': '''        
+        Integrate docnaet document in Partner form
+        Used for activate a page in Partner for have docnaet document available
+        there
+        ''',
+    'author': 'Micronaet S.r.l. - Nicola Riolini',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'docnaet',
+        ],
+    'init_xml': [],
+    'demo': [],
+    'data': [
+        'partner_view.xml',
+        ],
+    'active': False,
+    'installable': True,
+    'auto_install': False,
+    }
