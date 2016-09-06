8dff0bb0eadf528d16baa40eaab5cacaf9587e69
nicothebrush <nicola.riolini@gmail.com>
2016-09-06 16:50:36 +0200

    add log better

--- docnaet/agent/docnaet.py
+++ docnaet/agent/docnaet.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 import os
 import sys
+from datetime import datetime, timedelta
 #import subprocess
+
+
 
 # Parameters:
 # TODO parametrize:
 docnaet_path = 'C:\\Docnaet\\FileStore'
 docnaet_log = 'C:\\Docnaet\\Log\\docnaet.log'
 f_log = open(docnaet_log, 'a')
+date = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
 if len(sys.argv) != 2:
-    f_log.write('Not all parameters: %s\n' % (sys.argv))
+    f_log.write('%s [ERROR] Not all parameters: %s\n' % (date, sys.argv))
     sys.exit()
 
 # -----------------------------------------------------------------------------
@@ -34,11 +38,11 @@ if operation == 'open':
     # temp:
     document = os.path.join(docnaet_path, protocol_id, document_id) 
     command = 'start %s' % document
-    f_log.write('Command %s: %s\n' % (operation, command))
+    f_log.write('%s [INFO] %s: %s\n' % (date, operation, command))
     try:
         os.system(command)
     except:
-        f_log.write('Error launch command %s\n' % (sys.exc_info(), ))
+        f_log.write('%s [ERROR] Wrong command %s\n' % (date, sys.exc_info()))
     
 elif operation == 'home':
     pass # TODO
