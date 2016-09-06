6613ea571e4c372c2efc3ef9783a35c1a948005f
nicothebrush <nicola.riolini@gmail.com>
2016-09-06 18:20:26 +0200

    add comment

--- docnaet/agent/docnaet.py
+++ docnaet/agent/docnaet.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP) 
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+# This program run under windows for agent
+# maybe correct docnaet_path parametrize the creation also for linux systems
+#
+###############################################################################
+
+
 import os
 import sys
 import ConfigParser
@@ -22,6 +50,7 @@ except:
 # -----------------------------------------------------------------------------
 # Fixed parameters:
 # -----------------------------------------------------------------------------
+# NOTE: Program run only
 docnaet_path = 'C:\\Docnaet'
 docnaet_config_file = 'openerp.cfg'
 docnaet_config = os.path.join(docnaet_path, docnaet_config_file)
