5aa54490a347c1c7f595335012c33b281760993e
nicothebrush <nicola.riolini@gmail.com>
2017-04-22 17:14:13 +0200

    add type ID_tipologia in document, priority?

--- docnaet_remote_odbc_mdb/agent/odbc_creation.py
+++ docnaet_remote_odbc_mdb/agent/odbc_creation.py
@@ -216,7 +216,8 @@ convert_db = {
             's(record.name)', 's(record.description)', 's(record.note)',
             'record.number', 'record.fax_number', 
             's(record.docnaet_extension)', 's(record.date)', 
-            'mo(record.original_id)',          
+            'mo(record.original_id)', 'mo(record.type_id)',
+            #'priority_db.get(record.priority, 3)'
             ),
         (
             'ID_documento', 'ID_protocollo', 'ID_cliente', 
@@ -224,7 +225,8 @@ convert_db = {
             'docOggetto', 'docDescrizione', 'docNote',
             'docNumero', 'docFax', 
             'docEstensione', 'docData', 
-            'docFile',
+            'docFile', 'ID_tipologia',
+            #'ID_'
             ),
         ],
               
