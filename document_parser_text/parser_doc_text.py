13efc87807a276e3d92dda646020c6667b50126a
nicothebrush <nicola.riolini@gmail.com>
2017-07-19 08:52:48 +0200

    fast git

--- document_parser_text/parser_doc_text.py
+++ document_parser_text/parser_doc_text.py
@@ -36,6 +36,7 @@ from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
     float_compare)
 
 # Parse library:
+import pdb; pdb.set_trace()
 from subprocess import Popen, PIPE
 from docx import opendocx, getdocumenttext
 #http://stackoverflow.com/questions/5725278/python-help-using-pdfminer-as-a-library
