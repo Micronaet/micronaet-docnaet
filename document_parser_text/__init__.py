4bb7fdb61460d869dc5d013db8c1186ada8c0f57
nicothebrush <nicola.riolini@gmail.com>
2017-07-19 08:43:53 +0200

    fast git

--- document_parser_text/__init__.py
+++ document_parser_text/__init__.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from . import parse_text
+from . import parser_doc_text
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
