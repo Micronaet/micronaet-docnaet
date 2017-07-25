0dacbd04f952a415ff78f4ce40d1f740d6a97940
nicothebrush <nicola.riolini@gmail.com>
2017-07-25 12:11:58 +0200

    fast git

--- docnaet/agent/script/process.py
+++ docnaet/agent/script/process.py
@@ -19,7 +19,7 @@
 ###############################################################################
 import os
 import sys
-import wmi
+#import wmi
 import psutil
 import win32com.client    
 from win32gui import GetWindowText, GetForegroundWindow
