c6b142d137188dd2a00808d42bb292fdeff964c0
nicothebrush <nicola.riolini@gmail.com>
2018-02-12 12:23:01 +0100

    generic bugfix (fast message)

--- docnaet_remote/__init__.py
+++ docnaet_remote/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ###############################################################################
 #
 #    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
@@ -16,28 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
+#from . import 
 
-{
-    'name': 'Docnaet remote',
-    'version': '0.1',
-    'category': 'Docnaet',
-    'description': '''        
-        Docnaet: Remote manager for agents
-        ''',
-    'author': 'Micronaet S.r.l. - Nicola Riolini',
-    'website': 'http://www.micronaet.it',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
-        'docnaet_migration', # for manage original ID (for folders)
-        ],
-    'init_xml': [],
-    'demo': [],
-    'data': [
-        #'security/ir.model.access.csv',    
-        #'_views.xml', # after wizard      
-        ],
-    'active': False,
-    'installable': True,
-    'auto_install': False,
-    }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
