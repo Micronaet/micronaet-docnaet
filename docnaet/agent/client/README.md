015f37f5a78df38f5368c3a7b743a839e40f11e7
nicothebrush <nicola.riolini@gmail.com>
2018-04-19 17:03:26 +0200

    rewrite agent with [L] = Labnaet [D] or nothing = Docnaet

---
+++ docnaet/agent/client/README.md
@@ -0,0 +1,23 @@
+===============================================================================
+                       Manage open document in client
+===============================================================================
+
+Link mapping protocol: 
+openerp://
+
+===============================================================================
+                             Link format:
+===============================================================================
+1. Document:
+   openerp://document|ID.ext
+       >> Open file in filestore folder name id.ext
+
+2. Document remote [TODO]
+    openerp://document|id.ext[R]
+        >> Open file in filestore folder name id.ext in local filestore
+        
+3. Home folder:
+    openerp://folder|ID
+        >> Open private folder for user ID
+
+    
--- docnaet/agent/client/docnaet.py
+++ docnaet/agent/client/docnaet.py
@@ -3,15 +3,17 @@
 import os
 import sys
 import subprocess
-import ConfigParser
+from ConfigParser import ConfigParser
     
 # -----------------------------------------------------------------------------
-# Test remote or LAN mode (for choose config file):
+#                         PARSE COMMAND LAUNCH:
 # -----------------------------------------------------------------------------
-# Remote mode end: [R]
-command_line = sys.argv[1]
+# Read second part of command:
+command_line = sys.argv[1] # LAN/Local or Labnaet/Docnaet
 
+# -----------------------------------------------------------------------------
 # Check OS:
+# -----------------------------------------------------------------------------
 if os.name == 'posix': # Linux mode
     linux = True
     config_file = 'openerp.cfg'
@@ -19,37 +21,44 @@ else:
     linux = False
     config_file = 'openerp.cfg' # XXX now are the same, consider use "win."
 
-# Check remote mode call:
+# -----------------------------------------------------------------------------
+# Manage folder setup:
+# -----------------------------------------------------------------------------
+remote = False
 if command_line.endswith('[R]'):
+
+    # Check remote mode call:
     remote = True
     config_file = 'remote.%s' % config_file
     command_line = command_line[:-3] # Remove [R] part from command
-else:
-    remote = False
+elif command_line.endswith('[L]'):
+
+    # Check remote mode call:
+    config_file = 'labnaet.cfg'
+    command_line = command_line[:-3] # Remove [R] part from command
+else: # ends with [D] or nothing >> Docnaet 
+
+    # Check remote mode call:
+    #config_file = 'docnaet.cfg'
+    pass # XXX Nothing for now
+
+# Read config file:    
+cfg_file = os.path.join(current_path, config_file)
+config = ConfigParser()
+config.read([cfg_file])
 
 # -----------------------------------------------------------------------------
 #                                Parameters
 # -----------------------------------------------------------------------------
-current_path = os.path.expanduser(os.path.dirnamae(__file__))
-#current_path = os.path.expanduser(
-#    os.path.dirname(
-#        os.path.os.path.realpath(__file__)))
+# A. Static:
+close_tab = False # TODO put in config file
+current_path = os.path.expanduser(os.path.dirname(__file__))
 
-# Config file (home folder of current user):
-cfg_file = os.path.join(current_path, config_file)
-config = ConfigParser.ConfigParser()
-config.read([cfg_file])
+# B. File cfg selected:
 
 # Folder:
-if remote:
-    cfg_remote_file = os.path.join(current_path, 'remote.%s' % config_file)
-    config_remote = ConfigParser.ConfigParser()
-    config_remote.read([cfg_remote_file])
-    store_path = config.get('folder', 'store_path')
-else:
-    store_path = config.get('folder', 'store_path')
-
-private_path = config.get('folder', 'private_path') # not used in remote mode
+store_path = config.get('folder', 'store_path')
+private_path = config.get('folder', 'private_path')
 
 # Command
 protocol = config.get('command', 'protocol')
@@ -59,34 +68,36 @@ openfile_command = config.get('command', 'openfile_command')
 
 # Log:
 log_file = config.get('log', 'file')
+
+# Extra parameters:
 log_f = open(log_file, 'a')
 
 # -----------------------------------------------------------------------------
 # Parse elements:
 # -----------------------------------------------------------------------------
-command = sys.argv[1][len(protocol):].split(separator)
+command = command_line[len(protocol):].split(separator)
 if len(command) != 2:
-    log_f.write('Not enought parameters\n')
+    log_f.write(u'Not enought parameters: %s\n' % command_line)
     log_f.close()
     sys.exit()
+
+# Split 2 part of command:    
 operation, argument = command
 
-# ----------------------------------------------------------------------------
-# Operations: 
-# ----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+#                                 Operations: 
+# -----------------------------------------------------------------------------
 # -------------
 # 1. Open file:
 # -------------
 document_pid = False
-if operation == 'document':
+if operation.lower() == 'document':
     filename = os.path.join(store_path, argument)
     log_f.write('Open doc: %s\n' % filename)
     log_f.close()
     cmd = openfile_command % filename
     proc = subprocess.Popen(cmd.split(), shell=True) # XXX no extra space!!
     document_pid = proc.pid
-    #print 'Current Document PID: %s' % document_pid
-    #os.system(cmd)
 
 # ---------------
 # 2. Open folder:
@@ -103,35 +114,36 @@ elif not remote and operation == 'folder':
 # E: Error no correct operation
 # -----------------------------
 else:
-    log_f.write('Error not managed operation: %s (%s)\n' % (
+    log_f.write('Error operation not managed: %s (%s)\n' % (
         operation, 'remote' if remote else 'LAN'))
     log_f.close()
 
-# -----------------------------
-# T: Try closing TAB in Firefox
-# -----------------------------
-try:
-    # Need to be installed
-    import psutil
-    import win32com.client    
-    from win32gui import GetWindowText, GetForegroundWindow
-    #import wmi
-    
-    # Create scripting shell:
-    shell = win32com.client.Dispatch('WScript.Shell')            
-    pid_ids = [p.pid for p in psutil.process_iter() if 'firefox' in p.name()]
-    if pid_ids:
-        pid = pid_ids[-1] # last
-        shell.AppActivate(pid)
-        #print 'Kill Firefox PID: %s' % pid
-        shell.SendKeys('^{F4}') # CTRL + F4
+if close_tab:
+    # -------------------------------------------------------------------------
+    # T: Try closing TAB in Firefox
+    # -------------------------------------------------------------------------
+    try:
+        # Need to be installed
+        import psutil
+        import win32com.client    
+        from win32gui import GetWindowText, GetForegroundWindow
+        #import wmi
+        
+        # Create scripting shell:
+        shell = win32com.client.Dispatch('WScript.Shell')            
+        pid_ids = [
+            p.pid for p in psutil.process_iter() if 'firefox' in p.name()]
+        if pid_ids:
+            pid = pid_ids[-1] # last
+            shell.AppActivate(pid)
+            #print 'Kill Firefox PID: %s' % pid
+            shell.SendKeys('^{F4}') # CTRL + F4
        
-except:
-    log_f.write('No Win 32 com library (so no close TAB)')
+    except:
+        log_f.write('No Win 32 com library (so no close TAB)')
 
-# --------------------------
-# S: Show windows with focus
-# --------------------------
-if document_pid:
-    shell.AppActivate(document_pid)
-    
+    # -------------------------------------------------------------------------
+    # S: Show windows with focus
+    # -------------------------------------------------------------------------
+    if document_pid:
+        shell.AppActivate(document_pid)
