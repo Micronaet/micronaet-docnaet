14d30727b9cfdd1098676973d86970233125aca7
nicothebrush <nicola.riolini@gmail.com>
2019-08-06 17:39:15 +0200

    try external mail action

---
+++ docnaet_sale_excel/cron/mailer/mailer_report.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP) 
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import erppeek
+import ConfigParser
+import smtplib  
+
+# -----------------------------------------------------------------------------
+# Read configuration parameter:
+# -----------------------------------------------------------------------------
+cfg_file = os.path.expanduser('../openerp.cfg')
+
+config = ConfigParser.ConfigParser()
+config.read([cfg_file])
+
+# ERP Connection:
+odoo = {
+    'database': config.get('dbaccess', 'dbname'),
+    'user': config.get('dbaccess', 'user'),
+    'password': config.get('dbaccess', 'pwd'),
+    'server': config.get('dbaccess', 'server'),
+    'port': config.get('dbaccess', 'port'),
+    }
+
+# Mail:
+smtp = {
+    'subject': config.get('smtp', 'subject'),
+    'text': config.get('smtp', 'text'),
+    
+    'folder': config.get('smtp', 'folder'),
+    }
+
+# -----------------------------------------------------------------------------
+# Connect to ODOO:
+# -----------------------------------------------------------------------------
+
+odoo = erppeek.Client(
+    'http://%s:%s' % (odoo['server'], odoo['port']), 
+    db=odoo['database'],
+    user=odoo['user'],
+    password=odoo['password'],
+    )
+mailer = odoo.model('ir.mail_server')
+order = odoo.model('sale.order')
+
+# -----------------------------------------------------------------------------
+# SMTP Sent:
+# -----------------------------------------------------------------------------
+# Get mailserver option:
+mailer_ids = mailer.search([])
+if not mailer_ids:
+    print '[ERR] No mail server configured in ODOO'
+    sys.exit()
+
+odoo_mailer = mailer.browse(mailer_ids)[0]
+
+# Open connection:
+print '[INFO] Sending using "%s" connection [%s:%s]' % (
+    odoo_mailer.name,
+    odoo_mailer.smtp_host,
+    odoo_mailer.smtp_port,
+    )
+import pdb; pdb.set_trace()
+if odoo_mailer.smtp_encryption in ('ssl', 'starttls'):
+    smtp_server = smtplib.SMTP_SSL(
+        odoo_mailer.smtp_host, odoo_mailer.smtp_port)
+    #server_smtp.starttls()  
+else:
+    print '[ERR] Connect only SMTP SSL server!'
+    sys.exit()
+    #server_smtp.start() # TODO Check
+
+
+# Send mail:    
+smtp_server.login(odoo_mailer.smtp_user, odoo_mailer.smtp_pass)
+smtp_server.sendmail(odoo_mailer.smtp_user, smtp['to'], smtp['subject'])
+smtp_server.quit()
