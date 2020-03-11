e744f0ff629edfe0d73f911f5df75bbd98208d3e
nicothebrush <nicola.riolini@gmail.com>
2020-03-11 15:39:11 +0100

    generic bugfix (fast message)

--- docnaet/agent/xmlrpc/move/move_document.py
+++ docnaet/agent/xmlrpc/move/move_document.py
@@ -68,19 +68,23 @@ import pdb; pdb.set_trace()
 for document in document_pool.browse(document_ids):
     labnaet_id = document_pool.get_counter_labnaet_id()
     docnaet_id = document.id
-    
+        
+    # move file:
+    from_file = os.path.join(
+        from_path, 
+        '%s.%s' % (docnaet_id, docnaet.extension),
+        )
+    to_file =  os.path.join(
+        from_path, 
+        '%s.%s' % (labnaet_id, docnaet.extension),
+        )
+    shutil.copy(from_file, to_file)
+
     document_pool.write([document.id], {
         'docnaet_mode': 'labnaet',
         'labnaet_id': labnaet_id,
-        
         })
-    
-    # move file:
-    from_file = os.path.join(from_path, docnaet_id)
-    to_file =  os.path.join(from_path, labnaet_id)
-    
-    
-    shutil.copy(from_file, to_file)
+
     print 'Copy: %s > %s'
 
 import pdb; pdb.set_trace()
