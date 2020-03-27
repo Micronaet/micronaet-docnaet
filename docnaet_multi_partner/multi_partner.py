800c4da622f06f1764c899e929c2c61005888a06
nicothebrush <nicola.riolini@gmail.com>
2020-03-27 13:42:50 +0100

    better override

--- docnaet_multi_partner/multi_partner.py
+++ docnaet_multi_partner/multi_partner.py
@@ -50,28 +50,21 @@ class DocnaetDocument(orm.Model):
 
     # Override action partner_id hided
     def document_confirmed(self, cr, uid, ids, context=None):
-        ''' WF confirmed state
-        '''        
-        assert len(ids) == 1, 'Works only with one record a time'
-        data = {'state': 'confirmed'}
-        
-        protocol_pool = self.pool.get('docnaet.protocol')
-        current_proxy = self.browse(cr, uid, ids, context=context)[0]
-        if not current_proxy.number:
-            protocol = current_proxy.protocol_id
-            if not protocol:
-                raise osv.except_osv(
-                    _('Protocol'), 
-                    _('No protocol assigned, choose one before and confirm!'),
-                    )
-            if not current_proxy.partner_ids:
-                raise osv.except_osv(
-                    _('Partner'), 
-                    _('No partner assigned, choose one before and confirm!'),
-                    )
-            data['number'] = protocol_pool.assign_protocol_number(
-                cr, uid, protocol.id, context=context)
-        return self.write(cr, uid, ids, data, context=context)
+        ''' WF confirmed state (override with new check on partners)
+        '''
+        current = self.browse(cr, uid, ids, context=context)
+        if not current.docnaet_partner_ids:
+            raise osv.except_osv(
+                _('Partner'), 
+                _('No partners assigned, choose one before and confirm!'),
+                )
+        if not current.partner_id:
+            self.write(cr, uid, ids, {
+                'partner_id': partner.company_id.id,
+                }, context=context)      
+  
+        return super(DocnaetDocument,self).document_confirmed(
+            cr, uid, ids, context=context)
 
     _columns = {
         'docnaet_partner_ids': fields.many2many(
