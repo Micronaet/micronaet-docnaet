4a388dbf9789fd05c771ca17f1f8baf20595cfc6
nicothebrush <nicola.riolini@gmail.com>
2020-03-27 18:46:49 +0100

    Revert "add wizard to test!"
    This reverts commit 0afb47b6ff5d387c7502e2b9f6deeabdbabf1cbb.

--- docnaet_multi_product/wizard/import_data_sheet.py
+++ docnaet_multi_product/wizard/import_data_sheet.py
@@ -44,33 +44,27 @@ class docnaet_import_datasheet_wizard(orm.TransientModel):
         ''' Import management
         '''
         assert len(ids) == 1, 'Works only with one record a time'
-
-        log_text = ''            
+            
         wiz_proxy = self.browse(cr, uid, ids, context=context)[0]
         import pdb; pdb.set_trace()
         
-        path = os.path.expanduser(wiz_proxy.default_path)
+        default_path = wiz_proxy.default_path
         filename = wiz_proxy.filename
         
-        if not os.path.isdir(path):
-            log_text += 'No path found: %s' % path
-            _logger.error(log_text)
-            return True
-            
         # Pool used:
         document_pool = self.pool.get('docnaet.document')
         protocol_pool = self.pool.get('docnaet.protocol')
         partner_pool = self.pool.get('res.partner')
 
         if filename:
-            selected_files = [filename]
+            files = [filename]
         else:                 
-            for root, folders, files in os.walk(path):
-                selected_files = files
+            for root, folders, files in os.walk(path)
                 break  # Update files list only with current folder
 
         import pdb; pdb.set_trace()
-        for f in selected_files:
+        log_text = ''            
+        for f in files:
             if f.split('.')[-1] not in ('xls', 'xlsx'):
                 log_text.append('File not used: %s\n' % f)
                 continue
@@ -80,35 +74,13 @@ class docnaet_import_datasheet_wizard(orm.TransientModel):
             xls_file = os.path.join(path, f)        
             wb = xlrd.open_workbook(xls_file)
             for ws_name in wb.sheet_names():
-                # -------------------------------------------------------------
-                # Extract partner code:
-                # -------------------------------------------------------------
-                partner_code = ws_name.split()[-1]
-                # Check correct format
-                if partner_code[2:3] == '.' and partner_code[:2].isdigit() and\
-                        partner_code[3:].isdigit():
-                    partner_ids = partner_pool.search(cr, uid, [
-                        ('sql_supplier_code', '=', partner_code),
-                        ], context=context)
-                    if not partner_ids:
-                        log_text += 'Partner %s not found\n' % partner_code
-                        continue
-                    partner_id = partner_ids[0]
-                else:
-                    log_text += 'Sheet %s jumped, no partner code format\n' % (
-                        partner_code)
-                    continue    
-
-                # -------------------------------------------------------------
-                # Read hyperlink map:                
-                # -------------------------------------------------------------
                 ws = wb.sheet_by_name(ws_name)
                 for position in ws.hyperlink_map:
                     row, col = position
                     try:
                         cell = ws.cell(row, col)
                     except:
-                        log_text += 'Cell not found\n', f, ws_name, row, col
+                        print 'Cell not found', f, ws_name, row, col
                         continue
                     
                     link = ws.hyperlink_map[position]
@@ -116,18 +88,10 @@ class docnaet_import_datasheet_wizard(orm.TransientModel):
                         continue
 
                     file_link = os.path.join(path, link.url_or_path)
-                    log_text += 'Foglio %s, [%s:%s] Valore %s, Link: %s\n' % (
-                        ws_name, row, col, cell.value, file_link)
-
-        _logger.info(log_text)
-        return True               
+                    print ws_name, row, col, cell.value, file_link
                                 
     _columns = {
         'default_path': fields.char('Default path', size=180, required=True),
         'filename': fields.char('File name', size=180),
         }
-    
-    _defaults = {
-        'default_path': lambda *x: '~/quality/schede tecniche',
-        }    
 
