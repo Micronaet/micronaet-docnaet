b8c3effebb90e91f5fd73a37aa55ae2845540cc2
nicothebrush <nicola.riolini@gmail.com>
2020-06-23 13:46:27 +0200

    generic bugfix (fast message)

--- docnaet/script/document_search.py
+++ docnaet/script/document_search.py
@@ -1,10 +1,22 @@
 import os
 
 path = './'
-extensions = []
+extensions = [
+    'xls', 'xlsx',
+    'doc', 'docx',
+    'pdf',
+    # 'pub',
+    # 'ppt', 'pptx',
+    # 'pps', 'ppsx',
+    # 'zip',
+]
 
 files = []
 for root, folders, files in os.walk(path):
     for file in files:
+        extension = file.split('.')[-1]
         fullname = os.path.join(root, file)
-        print(fullname)
+        if extension in extensions:
+            print(fullname)
+        else:
+            print('[Jumped] %s' % fullname.replace('/', '|*|'))
