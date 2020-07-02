daad5aad35763cce3cafeddbd67bf6372ab52f12
nicothebrush <nicola.riolini@gmail.com>
2020-07-02 15:24:21 +0200

    pep 8 normalitazion

--- script/docnaet_import.py
+++ script/docnaet_import.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-# ODOO (ex OpenERP) 
+# ODOO (ex OpenERP)
 # Open Source Management Solution
 # Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
 # Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
@@ -12,7 +12,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
@@ -32,10 +32,11 @@ try:
     log_db = pickle.load(open(pickle_file, 'rb'))
 except:
     print('Log file not present, will be created: %s' % pickle_file)
-    log_db = {}    
+    log_db = {}
+
 
 # Utility:
-def xldate_to_datetime(xldatetime): 
+def xldate_to_datetime(xldatetime):
     """ Convert float, something like 43705.6158241088, to text date
     """
     import math
@@ -45,11 +46,12 @@ def xldate_to_datetime(xldatetime):
     (days, portion) = math.modf(xldatetime)
 
     delta_days = datetime.timedelta(days=days - 1)
-    #changing the variable name in the edit
+    # changing the variable name in the edit
     secs = int(24 * 60 * 60 * portion)
     delta_seconds = datetime.timedelta(seconds=secs)
     result = (start_date + delta_days + delta_seconds)
     return result.strftime("%Y-%m-%d %H:%M:%S")
+
 
 # -----------------------------------------------------------------------------
 # Read configuration parameter:
@@ -72,7 +74,7 @@ odoo_path = config.get('odoo', 'path')
 protocol_id = 2
 language_id = 1
 company_id = 1
-user_id = 30 # Gloria
+user_id = 30  # Gloria
 
 code_cell = 2
 sheet_cell = 3
@@ -85,12 +87,12 @@ selected_files = eval(config.get('server', 'files'))
 # -----------------------------------------------------------------------------
 odoo = erppeek.Client(
     'http://%s:%s' % (
-        server, port), 
+        server, port),
     db=dbname,
     user=user,
     password=pwd,
     )
-    
+
 # Pool used:
 SUPERUSER_ID = 1
 document_pool = odoo.model('docnaet.document')
@@ -100,12 +102,13 @@ mail_pool = odoo.model('mail.message')
 subtype_pool = odoo.model('mail.message.subtype')
 user_pool = odoo.model('res.users')
 
+
 def send_message(subject, body):
     """ Sends message to user_ids
     """
     discussion_ids = subtype_pool.search([
         ('name', '=', 'Discussions'),
-        ]) 
+        ])
     discussion = subtype_pool.browse(discussion_ids)[0]
     users = [user_pool.browse(SUPERUSER_ID)]
     mail_pool.create(values=dict(
@@ -116,11 +119,12 @@ def send_message(subject, body):
             body=body,
             ))
 
+
 program_db = {}
 program_ids = program_pool.search([])
 for program in program_pool.browse(program_ids):
     program_db[program.extension] = program.id
-            
+
 log_operation = 'Selezione da configurazione: [%s]' % (selected_files, )
 for root, folders, files in os.walk(path):
     for f in files:
@@ -128,20 +132,20 @@ for root, folders, files in os.walk(path):
         if f.split('.')[-1] not in ('xls', 'xlsx'):
             # log_operation += 'File non Excel: %s\n' % f
             continue
-        
+
         if f not in selected_files:
             # log_operation += 'File non nella selezione: %s' % f
             continue
         log_operation += 'File analizzato: %s\n' % f
 
-        xls_file = os.path.join(path, f)        
+        xls_file = os.path.join(path, f)
         if xls_file not in log_db:
             log_db[xls_file] = {}
         wb = xlrd.open_workbook(xls_file)
         sheet_index = {}
         for ws_name in wb.sheet_names():
             ws = wb.sheet_by_name(ws_name)
-            if ws_name == 'INDICE':     
+            if ws_name == 'INDICE':
                 for row in range(ws.nrows):
                     if not row:
                         continue # Jump first line
@@ -151,19 +155,19 @@ for root, folders, files in os.walk(path):
                     if not sheet_code:
                         log_file += 'No codice foglio riga: %s\n' % (row + 1)
                         continue
-                        
+
                     if not partner_code:
                         log_file += 'No codice fornitore riga: %s\n' % (row + 1)
                         continue
-                        
-                    if not(partner_code[:2].isdigit() and \
-                            partner_code[3:].isdigit() and \
+
+                    if not(partner_code[:2].isdigit() and
+                            partner_code[3:].isdigit() and
                             partner_code[2:3] == '.'):
                         log_file += 'Codce fornitore formato errato: %s\n' % \
                             partner_code
                         continue
                     partner_ids = partner_pool.search([
-                        ('sql_supplier_code', '=', partner_code)])    
+                        ('sql_supplier_code', '=', partner_code)])
 
                     if not partner_ids:
                         log_file += 'No codice partner in ODOO: %s\n' % \
@@ -171,19 +175,20 @@ for root, folders, files in os.walk(path):
                         continue
 
                     if len(partner_ids) > 1:
-                        log_file += 'Troppi partner in ODOO: %s\n' % partner_code
+                        log_file += 'Troppi partner in ODOO: %s\n' % \
+                                    partner_code
                         continue
 
-                    sheet_index[sheet_code] = partner_ids[0]                  
+                    sheet_index[sheet_code] = partner_ids[0]
                 continue # Next sheet
-            
+
             if ws_name not in sheet_index:
                 log_file += 'Foglio non in indice: %s\n' % ws_name
                 continue
             partner_id = sheet_index[ws_name]
             if ws_name not in log_db[xls_file]:
                 log_db[xls_file][ws_name] = {}
-                   
+
             for position in ws.hyperlink_map:
                 row, col = position
                 try:
@@ -192,7 +197,7 @@ for root, folders, files in os.walk(path):
                     log_file += 'Cella non trovata %s, [%s:%s]\n' % (
                         ws_name, row, col)
                     continue
-                
+
                 link = ws.hyperlink_map[position]
                 if link.type != 'url':
                     continue
@@ -201,7 +206,7 @@ for root, folders, files in os.walk(path):
                 supplier_code = ws.cell(row, 1).value
                 if type(supplier_code) == float:
                     supplier_code = str(int(supplier_code))
-                
+
                 try:
                     request_date = xldate_to_datetime(ws.cell(row, 2).value)
                 except:
@@ -215,20 +220,20 @@ for root, folders, files in os.walk(path):
                     date_value[:10],
                     )
                 note = 'Importato da file %s' % xls_file
-                        
+
                 file_link = os.path.join(path, link.url_or_path)
-                
+
                 if file_link not in log_db[xls_file][ws_name]:
                     log_db[xls_file][ws_name][file_link] = False # Docnaet ID
-                    
+
                 docnaet_extension = file_link.split('.')[-1].lower()
                 program_id = program_db.get(docnaet_extension)
                 if not program_id:
-                    log_file += 'Estension not found %s\n' % docnaet_extension
-                    
+                    log_file += 'Extension not found %s\n' % docnaet_extension
+
                 odoo_data = {
                     'name': name,
-                    'supplier_code': supplier_code,                    
+                    'supplier_code': supplier_code,
                     'protocol_id': protocol_id,
                     'description': description,
                     'note': note,
@@ -246,25 +251,24 @@ for root, folders, files in os.walk(path):
                     'docnaet_mode': 'docnaet',
                     'imported': True,
                     'program_id': program_id,
-                    #'priority': 'normal',
-                    #'state': 'draft',
+                    # 'priority': 'normal',
+                    # 'state': 'draft',
                     }
-                
-                if log_db[xls_file][ws_name][file_link]: # Update
+
+                if log_db[xls_file][ws_name][file_link]:  # Update
                     document_pool.write(
                         log_db[xls_file][ws_name][file_link], odoo_data)
                 else:
                     log_db[xls_file][ws_name][file_link] = \
                         document_pool.create(odoo_data).id
-                    pickle.dump(log_db, open(pickle_file, 'wb')) # History
+                    pickle.dump(log_db, open(pickle_file, 'wb'))  # History
 
-                # -------------------------------------------------------------                    
-                # File operations:    
-                # -------------------------------------------------------------                    
+                # -------------------------------------------------------------
+                # File operations:
+                # -------------------------------------------------------------
                 odoo_id = log_db[xls_file][ws_name][file_link]
                 odoo_file = os.path.join(
                     odoo_path, '%s.%s' % (odoo_id, docnaet_extension))
-
 
                 if os.path.isfile(odoo_file):
                     # log_file += 'File presente in ODOO %s\n' % odoo_file
