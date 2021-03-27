19b98c3d84002658bd4ebf2a04ad892d0501dc34
nicothebrush <nicola.riolini@gmail.com>
2021-03-27 09:02:31 +0100

    generic bugfix (fast message)

--- script/docnaet2excel/extract_docnaet_to_excel.py
+++ script/docnaet2excel/extract_docnaet_to_excel.py
@@ -168,7 +168,8 @@ for item in sorted(database['document'], key=lambda x: (
         item['docData'], item['docScadenza'],         
         partner_id, category_id, country_id,         
         type_id, language_id, application_id, user_id,        
-        item['docOggetto'], item['docDescrizione'], item['docNote'],
+        unicode(item['docOggetto']), unicode(item['docDescrizione']), 
+        unicode(item['docNote']),
         item['docFile'], extension, item['docCreazioneEffettiva'],              
     ]
 
