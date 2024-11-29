d7b829f328ca78ec8c9acdbc2a471d9e2c018a02
nicothebrush <nicola.riolini@gmail.com>
2024-11-29 09:43:04 +0100

    generic bugfix (fast message)

--- docnaet/agent/DockerFlask/batch/update.bat
+++ docnaet/agent/DockerFlask/batch/update.bat
@@ -1,13 +1,13 @@
 rem Run this batch in folder where Dockerfile is!
 
 rem Parameters:
-docker_folder=c:\Micronaet\Docker
-root_folder=%docker_folder%\DockerFlask
-git_folder=%docker_folder%\git\micronaet-docnaet
-data_folder=%root_folder%\data
+SET docker_folder="c:\Micronaet\Docker"
+SET root_folder="%docker_folder%\DockerFlask"
+SET git_folder="%docker_folder%\git\micronaet-docnaet"
+SET data_folder="%root_folder%\data"
 
 rem Update from git:
-mkdir %git_folder%
+mkdir "%git_folder%"
 git pull
 
 rem Create Docker environment
@@ -20,11 +20,11 @@ docker stop docnaet-agent-flask
 docker rm docnaet-agent-flask
 docker rmi docnaet-agent-flask
 
-# Create new image from Dockerfile:
+rem Create new image from Dockerfile:
 docker build --tag docnaet-agent-flask .
 
-# Restart new container:
+rem Restart new container:
 docker run -d -p 5000:5000 --name=docnaet-agent --hostname=docnaet -v %data_folder%:/docnaet/data --restart=always docnaet-agent-flask
 
-# Show started container:
+rem Show started container:
 docker ps -a
