f5ebccc607c68ed7dfe7611bb8c7db12f111a119
nicothebrush <nicola.riolini@gmail.com>
2024-11-29 09:56:32 +0100

    generic bugfix (fast message)

--- docnaet/agent/DockerFlask/batch/update.bat
+++ docnaet/agent/DockerFlask/batch/update.bat
@@ -11,7 +11,7 @@ mkdir "%git_folder%"
 git pull
 
 rem Create Docker environment
-copy /s/d "%git_folder%\docnaet\client\DockerFlask" "%root_folder%"
+xcopy "%git_folder%\docnaet\client\DockerFlask" "%root_folder%" /e/d/y
 mkdir %data_folder%
 cd %root_folder%
 
