4fe35ddce0dff65e9fb106eda63f476bfe44a368
nicothebrush <nicola.riolini@gmail.com>
2024-11-29 11:20:59 +0100

    generic bugfix (fast message)

--- docnaet/agent/Flask/batch/update.bat
+++ docnaet/agent/Flask/batch/update.bat
@@ -5,13 +5,18 @@ SET micronaet_folder=C:\Micronaet
 SET app_folder=%micronaet_folder%\Docnaet\Flask
 SET git_folder=%micronaet_folder%\git\micronaet-docnaet
 SET data_folder=%app_folder%\Data
+SET requirements=%app_folder%\requirements.txt
 
-SET python_folder=C:\Python2.7
+SET python_folder=C:\Python34
 SET python_command=%python_folder%\python.exe
+SET pip_command=%python_folder%\Scripts\pip.exe
 
 rem Update from git:
 cd %git_folder%
 git pull
+
+rem Install
+%pip_command% install --no-cache-dir --upgrade -r %requirements%
 
 rem Create Docker environment
 xcopy %git_folder%\docnaet\agent\Flask\* %app_folder%\* /e/d/y
