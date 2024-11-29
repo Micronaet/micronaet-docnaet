af9257ef0dc136bb39636bed89626fd7eee12396
nicothebrush <nicola.riolini@gmail.com>
2024-11-29 11:16:07 +0100

    generic bugfix (fast message)

---
+++ docnaet/agent/Flask/batch/git_update.bat
@@ -0,0 +1,21 @@
+rem Run this batch in folder where Dockerfile is!
+
+rem Parameters:
+SET micronaet_folder=C:\Micronaet
+SET app_folder=%micronaet_folder%\Docnaet\Flask
+SET git_folder=%root_folder%\git\micronaet-docnaet
+SET data_folder=%app_folder%\Data
+
+SET python_folder=C:\Python2.7
+SET python_command=%python_folder%\python.exe
+
+rem Update from git:
+cd %git_folder%
+git pull
+
+rem Create Docker environment
+xcopy %git_folder%\docnaet\agent\Flask\* %app_folder%\* /e/d/y
+mkdir %data_folder%
+cd %app_folder%
+
+pause
