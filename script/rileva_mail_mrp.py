3d304711f9fdd32ca7ac7a734c70009fe4789a48
nicothebrush <nicola.riolini@gmail.com>
2025-02-19 10:24:57 +0100

    add report script MRP for Edg

--- script/rileva_mail_mrp.py
+++ script/rileva_mail_mrp.py
@@ -25,11 +25,19 @@ import erppeek
 import xlsxwriter
 import ConfigParser
 
+import smtplib
+from datetime import datetime
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.mime.text import MIMEText
+from email import Encoders
+
 # -----------------------------------------------------------------------------
 # Read configuration parameter:
 # -----------------------------------------------------------------------------
 # Excel file: 
 file_out = './carichi_di_produzione.xlsx'
+now = ('%s' % datetime.now())[:19]
 
 # From config file:
 cfg_file = os.path.expanduser('../openerp.cfg')
@@ -42,6 +50,11 @@ pwd = config.get('dbaccess', 'pwd')
 server = config.get('dbaccess', 'server')
 port = config.get('dbaccess', 'port')   # verify if it's necessary: getint
 
+mail_recipients = config.get('smtp', 'to')
+mail_text = ''
+mail_subject = ''
+
+
 # -----------------------------------------------------------------------------
 # Connect to ODOO:
 # -----------------------------------------------------------------------------
@@ -52,7 +65,9 @@ odoo = erppeek.Client(
     user=user,
     password=pwd,
     )
-load_pool = odoo.model('mrp.production.workcenter.line')	
+mailer = odoo.model('ir.mail_server')
+
+load_pool = odoo.model('mrp.production.workcenter.line')
 
 # Excel:
 WB = xlsxwriter.Workbook(file_out)
@@ -75,7 +90,9 @@ load_ids = load_pool.search([
 total = len(load_ids)
 loads = sorted(
     load_pool.browse(load_ids), 
-    key=lambda x: (x.product.default_code, x.start_date))
+    key=lambda x: x.date_start,
+    reverse=True
+)
     
 for load in loads:
     row += 1
@@ -101,3 +118,44 @@ try:
     WB.close()
 except:
     print('Errore chiudendo file XLSX')
+
+# -----------------------------------------------------------------------------
+#                                  SMTP Sent:
+# -----------------------------------------------------------------------------
+# Get mailserver option:
+mailer_ids = mailer.search([])
+if not mailer_ids:
+    print('[ERR] No mail server configured in ODOO')
+    sys.exit()
+
+odoo_mailer = mailer.browse(mailer_ids)[0]
+
+# Open connection:
+print('[INFO] Sending using "%s" connection [%s:%s]' % (
+    odoo_mailer.name,
+    odoo_mailer.smtp_host,
+    odoo_mailer.smtp_port,
+    ))
+
+if odoo_mailer.smtp_encryption in ('ssl', 'starttls'):
+    smtp_server = smtplib.SMTP_SSL(
+        odoo_mailer.smtp_host, odoo_mailer.smtp_port)
+else:
+    print('[ERR] Connect only SMTP SSL server!')
+    sys.exit()
+    # server_smtp.start() # TODO Check
+
+smtp_server.login(odoo_mailer.smtp_user, odoo_mailer.smtp_pass)
+for to in mail_recipients.replace(' ', '').split(','):
+    print('Sending mail to: %s ...' % to)
+    msg = MIMEMultipart()
+    msg['Subject'] = mail_subject
+    msg['From'] = odoo_mailer.smtp_user
+    msg['To'] = mail_recipients
+    msg.attach(MIMEText(mail_text, 'html'))
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(open(file_out, 'rb').read())
+    Encoders.encode_base64(part)
+    part.add_header(
+        'Content-Disposition', 'attachment; filename="%s"' % filename)
