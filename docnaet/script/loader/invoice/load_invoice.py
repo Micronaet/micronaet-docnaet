d6afcddad4292afa24a5d9126e253e59b71a5214
nicothebrush <nicola.riolini@gmail.com>
2025-05-26 09:56:41 +0200

    Fast commit

--- docnaet/script/loader/invoice/load_invoice.py
+++ docnaet/script/loader/invoice/load_invoice.py
@@ -78,7 +78,7 @@ default_data = {
 def get_name(invoice_ref, date):
     """ Return invoice name if correct format: “8/xxx del 05.05.25”
     """
-    return '{}/{} del {}.{}.{}'.foramat(
+    return '{}/{} del {}.{}.{}'.format(
         int(invoice_ref[2:4]),
         int(invoice_ref[5:4]),
 
